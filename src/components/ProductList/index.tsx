17741278373bebe5aba22c83aef828a400c924c7
carlosvbp <CarlosVinicius7345@gmail.com>
2024-04-08 20:48:28 -0300

    ProductList

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import styles from "./style.module.scss"
 import { ProductContext } from "../../providers/Context";
+import { Product } from "../../interfaces/products.interface";
 
 export const ProductList = () => {
     const { addCart, productsFilter } = useContext(ProductContext)
    return (
       <ul className={styles.content}>
-         {productsFilter.map((product) => (
+         {productsFilter.map((product: Product) => (
             <ProductCard key={product.id} product={product} 
             addCart={addCart}/>
          ))}
