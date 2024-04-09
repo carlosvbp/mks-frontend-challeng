d5d642924cf695e9619d167b90137e40372f2c53
carlosvbp <CarlosVinicius7345@gmail.com>
2024-04-09 12:58:25 -0300

    commit

--- src/components/CartModal/CartItemCard/index.tsx
+++ src/components/CartModal/CartItemCard/index.tsx
@@ -5,11 +5,25 @@ import { ProductContext } from "../../../providers/Context";
 import { useContext } from "react";
 
 interface ProductCardProps {
-    product: Product;
+   product: Product;
 }
 
-export const CartItemCard = ( {product}: ProductCardProps ) => {
-    const { removeCart } = useContext(ProductContext)
+export const CartItemCard = ({ product }: ProductCardProps) => {
+   const { removeCart } = useContext(ProductContext)
+
+   /* const addProduct = () => {
+      const existingProductIndex = cartList.findIndex((item) => item.id === product.id);
+
+      if (existingProductIndex !== -1) {
+
+         const updatedCartList = [...cartList];
+         updatedCartList[existingProductIndex].count++; 
+
+         setCartList(updatedCartList);
+      } else {
+         addCart(product);
+      }
+   }; */
 
    return (
       <li className={styles.card}>
@@ -18,11 +32,19 @@ export const CartItemCard = ( {product}: ProductCardProps ) => {
                <img src={product.photo} alt={product.name} />
             </div>
             <h3>{product.name}</h3>
-            <h3>{product.price.toLocaleString('pt-BR', { style: "currency", currency: "BRL" })}</h3>
+            <div>
+               <span>Qt:</span>
+               {/* <div>
+                  <button> - </button>
+                  <h3>{product.count}</h3>
+                  <button> + </button>
+               </div> */}
+            </div>
+            <h3>{Number(product.price).toLocaleString('pt-BR', { style: "currency", currency: "BRL" })}</h3>
          </div>
          <button onClick={() => removeCart(product.id)} aria-label="delete" title="Remover item">
-               <MdClose size={15} />
-            </button>
+            <MdClose size={15} />
+         </button>
       </li>
    );
 };
--- src/interfaces/products.interface.ts
+++ src/interfaces/products.interface.ts
@@ -5,4 +5,5 @@ export interface Product {
     description: string
     price: number
     photo: string
+    count: number
 }
